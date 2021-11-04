a5d012b7957050090644af90cd5f2501680383bf
iojhan <iojabal9@gmail.com>
2021-11-04 14:12:13 -0400

    tipos de numeros

--- src/main.go
+++ src/main.go
@@ -3,6 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("HOla mundo")
+	//Declaracion de constantes
+	const pi float64 = 3.149516
+	const p2 = 3.149616
+	fmt.Println("pi", pi)
+	fmt.Println("pi2:", p2)
+	//declaracion de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+	//zero values
+	var a int
+	var b float32
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+	//Area de un cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("el area del cuadrado es igual a: ", areaCuadrado)
 
 }
