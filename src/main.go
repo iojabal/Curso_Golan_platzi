edd55b1e250b74e6666d26fcac9d720f0d6f87d6
iojhan <iojabal9@gmail.com>
2021-11-04 14:21:16 -0400

    structs

--- src/main.go
+++ src/main.go
@@ -2,28 +2,21 @@ package main
 
 import "fmt"
 
+type Car struct {
+	brand string
+	year  int
+}
+
 func main() {
-	//Declaracion de constantes
-	const pi float64 = 3.149516
-	const p2 = 3.149616
-	fmt.Println("pi", pi)
-	fmt.Println("pi2:", p2)
-	//declaracion de variables enteras
-	base := 12
-	var altura int = 14
-	var area int
+	myCar := Car{
+		brand: "Ford",
+		year:  2020,
+	}
+	fmt.Println(myCar)
 
-	fmt.Println(base, altura, area)
-	//zero values
-	var a int
-	var b float32
-	var c string
-	var d bool
+	//otra manera
+	otherCar := Car{brand: "Toyota", year: 1190}
+	otherCar.brand = "Honda"
 
-	fmt.Println(a, b, c, d)
-	//Area de un cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
-	fmt.Println("el area del cuadrado es igual a: ", areaCuadrado)
-
+	fmt.Println(otherCar)
 }
